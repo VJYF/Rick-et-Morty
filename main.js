cee80720ae35701416cf922d7dfa4db2fc164dbc
VJYF <vincent.jimenez.fernandez@hotmail.com>
2022-12-28 17:38:03 +0100

    Code au propre

--- main.js
+++ main.js
@@ -1,62 +1,15 @@
-// const image = document.getElementsByClassName('Cardimage');
-
-//const todosHTML = document.getElementById("todosHTML");
-// const getTodos = () => {
-//     fetch("https://jsonplaceholder.typicode.com/todos")
-//         .then((Response) => Response.json())
-//         .then((todos) => {
-//             fetch("https://jsonplaceholder.typicode.com/users")
-//                 .then((Response) => Response.json())
-//                 .then((users) => {
-//                     users.forEach((user) => {
-
-//                         var newDiv = document.createElement("table");
-
-//                         todos.forEach((todo) => {
-//                             if (todo.userId == user.id) {
-//                                 newDiv.innerHTML += `
-                                
-//                                 <tr class="headtr">
-//                                 <th class="th">${todo.userId = user.id ? user.name : "ok"}</th>
-//                                 <th class="th">${todo.id}</th>
-//                                 <td class="td">${todo.title}</td>
-//                                 <td class="td">${todo.completed}</td>
-//                                 </tr>
-//                                 `
-
-//                                 todosHTML.appendChild(newDiv);
-//                                 console.log(users.name);
-//                             };
-//                         })
-//                     });
-//                 });
-//         });
-// };
-// getTodos();
-
-
-const Appmain = document.getElementById('Appmain')
-const Cardimg = document.getElementsByClassName('Cardimage');
-const Cardname = document.getElementsByClassName('Cardimage');
-const Cardtext2 = document.getElementsByClassName('Cardtext2');
-// const locations = document.getElementById('location');
-
+const Appmain = document.getElementById('Appmain');
+character = "https://rickandmortyapi.com/api/character";
 const getCharacters = () => {
-    fetch("https://rickandmortyapi.com/api/character")
+    fetch(character)
     .then((Response) => Response.json())
     .then((characters) => {
-        // console.log(characters['results'])
-        // characters = characters['results']
         var main = document.createElement("div");
         characters["results"].forEach((character => {
-            // console.log(character)
-            searchPic = character.image;
-            //console.log(searchPic)
- 
-                main.innerHTML += `
-                        <section class="CharactersList">
+            main.innerHTML += `
+                        <section class="CharactersList" id="CharactersList">
                             <div id="character_card" class="Card CharactersListcard">
-                                <div class="Cardmain">
+                                <div class="Cardmain" id="${character.name}">
                                     <div class="Cardimage-container">
                                         <img id="Cardimage" class="Cardimage" src="${character.image}">
                                     </div>
@@ -76,25 +29,52 @@ const getCharacters = () => {
                                     <p class="Cardepisodes">
                                     <b class="Cardlabel">Played in ${character.episode.length} episodes: </b>
                                     </p>
-                                    </div>`
-                                    fetch("https://rickandmortyapi.com/api/episode/")
-                                    .then((Response) => Response.json())
-                                    .then((episodes) => {
-                                        episodes['result'].forEach(episode => {
-                                            
-                                        `<ul class="EpisodesList" id="EpisodesList">
-                                        </ul>`
-                                        });
-                                        
-                                    })
-                                    `
+                                </div>
                             </div>    
                         </section>
                             `
             Appmain.appendChild(main);
-
-            
         }));
     })
 }
+
+const getEpisodes = () => {
+    fetch(character)
+    .then((Response) => Response.json())
+    .then((characters) => {
+        characters.results.forEach((character => {
+            character.episode.forEach(episode => {
+                const CharactersList = document.getElementById(character.name);
+                fetch(episode)
+                .then((Response) => Response.json())
+                .then((episodesbyid) => {
+                    console.log(episodesbyid)
+                    var CharactersListEpisodes = document.createElement("div");
+                    CharactersListEpisodes.innerHTML +=
+                    `
+                    <ul>
+                    <li>
+                    <p>${episodesbyid.episode} - ${episodesbyid.name}</p>
+                    </li>
+                    </ul>
+                    `
+                    CharactersList.appendChild(CharactersListEpisodes);
+
+                    });
+                });
+            }));
+        });
+}   
+
 getCharacters();
+getEpisodes();
+
+
+function load(a) {  
+    a += 1
+    console.log("a : " + a); 
+    character = "https://rickandmortyapi.com/api/character?page=" + a;
+    console.log("page : " + character)
+    getCharacters();
+    getEpisodes();
+  }
